ea26311152bddd57c2085f8f8928061af1b54089
jake <jacopo.r00ta@gmail.com>
2020-04-15 23:36:34 +0200

    update project structure and pipelines

---
+++ platform/user-service/src/test/java/com/r00ta/telematics/platform/mocks/UserServiceMock.java
@@ -0,0 +1,69 @@
+package com.r00ta.telematics.platform.mocks;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.r00ta.telematics.platform.routes.models.Route;
+import com.r00ta.telematics.platform.users.IUserService;
+import com.r00ta.telematics.platform.users.models.EnrichedTripSummary;
+import com.r00ta.telematics.platform.users.models.LiveTrip;
+import com.r00ta.telematics.platform.users.models.User;
+import com.r00ta.telematics.platform.users.models.UserStatistics;
+
+public class UserServiceMock implements IUserService {
+
+    private Map<String, User> storage = new HashMap<>();
+
+    private Map<String, List<String>> newsStorage = new HashMap<>();
+
+    @Override
+    public User getUserById(String user) {
+        return storage.get(user);
+    }
+
+    @Override
+    public boolean createUser(User user) {
+        storage.put(user.userId, user);
+        return true;
+    }
+
+    @Override
+    public UserStatistics getUserOverview(String userId) {
+        return null;
+    }
+
+    @Override
+    public List<String> getUserNews(String userId) {
+        if (newsStorage.containsKey(userId)){
+            return newsStorage.get(userId);
+        }
+        return new ArrayList<>();
+    }
+
+    @Override
+    public boolean storeNews(String userId, String news) {
+        if (newsStorage.containsKey(userId)){
+            return newsStorage.get(userId).add(news);
+        }
+        newsStorage.put(userId, new ArrayList<>());
+        newsStorage.get(userId).add(news);
+        return true;
+    }
+
+    @Override
+    public boolean processScore(EnrichedTripSummary enrichedTripSummary) {
+        return false;
+    }
+
+    @Override
+    public boolean storeUserStatisticsDocument(String userId, UserStatistics userStatistics) {
+        return false;
+    }
+
+    @Override
+    public boolean notifyLiveTrip(LiveTrip liveTrip) {
+        return false;
+    }
+}
--- scoring-service/src/test/java/com/r00ta/telematics/platform/NativescoringIT.java
+++
@@ -1,9 +0,0 @@
-package com.r00ta.telematics.platform;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativescoringIT extends scoringTest {
-
-    // Execute the same tests but in native mode.
-}
