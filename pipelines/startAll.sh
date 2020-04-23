bbb2ca172f7e884bdf5f9194de1eb4a5f7498a0e
jake <jacopo.r00ta@gmail.com>
2020-04-23 19:32:15 +0200

    add jabba to switch to java11

--- pipelines/startAll.sh
+++ pipelines/startAll.sh
@@ -1,3 +1,8 @@
+curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
+
+jabba install adopt@1.11.0-6
+jabba use adopt@1.11.0-6
+
 cd platform
 cd live-trip-service/target
 java -jar *runner.jar &
