681cb1a26759ab702e38b2f0e88926fb5ace4062
jake <jacopo.r00ta@gmail.com>
2020-04-25 19:34:55 +0200

    rename docker images

--- pipelines/build-docker-images.sh
+++ pipelines/build-docker-images.sh
@@ -1,11 +1,11 @@
 cd platform
 cd live-trip-service
-docker build -f src/main/docker/Dockerfile.jvm -t quarkus/live-trip-service-jvm .
+docker build -f src/main/docker/Dockerfile.jvm -t r00ta/live-trip-service-jvm .
 
 cd ../
 cd scoring-service
-docker build -f src/main/docker/Dockerfile.jvm -t quarkus/scoring-service-jvm .
+docker build -f src/main/docker/Dockerfile.jvm -t r00ta/scoring-service-jvm .
 
 cd ..
 cd user-service
-docker build -f src/main/docker/Dockerfile.jvm -t quarkus/user-service-jvm .
+docker build -f src/main/docker/Dockerfile.jvm -t r00ta/user-service-jvm .
